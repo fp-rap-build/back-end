bbcfeb43c3b8ab337dbf1fefb20b76483f82a27f
Christopher Harwell <chrisharwell72@gmail.com>
2021-01-28 22:28:46 -0500

    adds seed data to address table

--- data/seeds/003_address.js
+++ data/seeds/003_address.js
@@ -1,10 +1,10 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('address').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
+      return knex('address').insert([
         {id: 1, streetNumber: '', streetName: '', cityName: '', state: '', zipCode: ''}
       ]);
     });
