46207ab875144cf50c64f33fd7b62a221bdb332d
isaiah <greatpup1940@gmail.com>
2021-01-29 13:49:09 -0500

    Changed Knex config to use different database urls

--- config/knexfile.js
+++ config/knexfile.js
@@ -5,7 +5,7 @@ dotenv.config({ path: "../.env" });
 module.exports = {
   development: {
     client: "pg",
-    connection: process.env.DATABASE_URL,
+    connection: process.env.DATABASE_URL_DEV,
     migrations: { directory: "../data/migrations" },
     seeds: { directory: "../data/seeds" },
     pool: {
@@ -16,14 +16,14 @@ module.exports = {
 
   test: {
     client: "pg",
-    connection: process.env.DATABASE_URL,
+    connection: process.env.DATABASE_URL_DEV,
     migrations: { directory: "../data/migrations" },
     seeds: { directory: "../data/seeds" },
   },
 
   production: {
     client: "pg",
-    connection: process.env.DATABASE_URL,
+    connection: process.env.DATABASE_URL_PROD,
     migrations: { directory: "../data/migrations" },
     seeds: { directory: "../data/seeds" },
   },
