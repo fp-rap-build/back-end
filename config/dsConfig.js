b42f50a396d7a7e5ccd2437e16643911b591cf3d
isaiah <greatpup1940@gmail.com>
2021-01-29 02:13:45 -0500

    Added seeds for the users

--- config/dsConfig.js
+++ config/dsConfig.js
@@ -1,5 +1,6 @@
-var dotenv = require('dotenv');
-dotenv.config({ path: '../.env' });
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "../.env" });
 
 module.exports = {
   baseURL: process.env.DS_API_URL,
--- config/knexfile.js
+++ config/knexfile.js
@@ -1,12 +1,13 @@
-var dotenv = require('dotenv');
-dotenv.config({ path: '../.env' });
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "../.env" });
 
 module.exports = {
   development: {
-    client: 'pg',
+    client: "pg",
     connection: process.env.DATABASE_URL,
-    migrations: { directory: '../data/migrations' },
-    seeds: { directory: '../data/seeds' },
+    migrations: { directory: "../data/migrations" },
+    seeds: { directory: "../data/seeds" },
     pool: {
       min: 2,
       max: 10,
@@ -14,16 +15,16 @@ module.exports = {
   },
 
   test: {
-    client: 'pg',
+    client: "pg",
     connection: process.env.DATABASE_URL,
-    migrations: { directory: '../data/migrations' },
-    seeds: { directory: '../data/seeds' },
+    migrations: { directory: "../data/migrations" },
+    seeds: { directory: "../data/seeds" },
   },
 
   production: {
-    client: 'pg',
+    client: "pg",
     connection: process.env.DATABASE_URL,
-    migrations: { directory: '../data/migrations' },
-    seeds: { directory: '../data/seeds' },
+    migrations: { directory: "../data/migrations" },
+    seeds: { directory: "../data/seeds" },
   },
 };
