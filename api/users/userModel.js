470498753b5d6d276927a2729743c9d39907fceb
Christopher Harwell <chrisharwell72@gmail.com>
2021-01-31 16:13:49 -0500

    Adds query to get all user data

--- api/users/userModel.js
+++ api/users/userModel.js
@@ -1,5 +1,15 @@
 const db = require("../../data/db-config");
 
+const findAllUserData = async () => {
+    await db("users")
+        .join("name", "users.id", "=", "name.user_id")
+        .join("rental_assistance", "users.id", "=", "rental_assistance.user_id" )
+        .join("roles", "users.id", "=", "roles.user_id")
+        .join("family_size", "users.id", "=", "family_size.user_id")
+        .join("address", "users.id", "=", "address.user_id")
+        .returning("*")
+}
+
 const findAll = async () => await db("users");
 
 const findBy = (filter) => db("users").where(filter);
@@ -36,4 +46,5 @@ module.exports = {
   update,
   remove,
   findOrCreateProfile,
+  findAllUserData
 };
