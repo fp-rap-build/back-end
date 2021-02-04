7d5ae0f03d9ecbbb319d7466364b2f053cffe1ad
joeywire <61367542+joeywire@users.noreply.github.com>
2021-02-04 17:01:00 -0600

    Organizations router built

---
+++ api/organizations/org-model.js
@@ -0,0 +1,29 @@
+const db = require("../../data/db-config");
+
+const findAll = () => {
+  return db("organizations");
+};
+
+const findById = (id) => {
+  return db("organizations").where("id", id).first();
+};
+
+const create = (org) => {
+  return db("organizations").insert(org).returning("*");
+};
+
+const update = (id, org) => {
+  return db("organizations").where("id", id).update(org);
+};
+
+const remove = (id) => {
+  return db("organizations").where("id", id).del();
+};
+
+module.exports = {
+  findAll,
+  findById,
+  create,
+  update,
+  remove,
+};
