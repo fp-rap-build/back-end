0cebb106714051321e694ab751bf3cefeb2f55f2
Isaiah <isaiahjfowler7@gmail.com>
2021-02-07 20:39:59 -0500

    Deleted unused code

--- api/routes/users/userRouter.js
+++ api/routes/users/userRouter.js
@@ -12,65 +12,6 @@ router.get("/me", authRequired, (req, res) => {
   });
 });
 
-/**
- * @swagger
- * components:
- *  schemas:
- *    Profile:
- *      type: object
- *      required:
- *        - id
- *        - email
- *        - name
- *        - avatarUrl
- *      properties:
- *        id:
- *          type: string
- *          description: This is a foreign key (the okta user ID)
- *        email:
- *          type: string
- *        name:
- *          type: string
- *        avatarUrl:
- *          type: string
- *          description: public url of profile avatar
- *      example:
- *        id: '00uhjfrwdWAQvD8JV4x6'
- *        email: 'frank@example.com'
- *        name: 'Frank Martinez'
- *        avatarUrl: 'https://s3.amazonaws.com/uifaces/faces/twitter/hermanobrother/128.jpg'
- *
- * /Users:
- *  get:
- *    description: Returns a list of Users
- *    summary: Get a list of all Users
- *    security:
- *      - okta: []
- *    tags:
- *      - profile
- *    responses:
- *      200:
- *        description: array of Users
- *        content:
- *          application/json:
- *            schema:
- *              type: array
- *              items:
- *                $ref: '#/components/schemas/Profile'
- *              example:
- *                - id: '00uhjfrwdWAQvD8JV4x6'
- *                  email: 'frank@example.com'
- *                  name: 'Frank Martinez'
- *                  avatarUrl: 'https://s3.amazonaws.com/uifaces/faces/twitter/hermanobrother/128.jpg'
- *                - id: '013e4ab94d96542e791f'
- *                  email: 'cathy@example.com'
- *                  name: 'Cathy Warmund'
- *                  avatarUrl: 'https://s3.amazonaws.com/uifaces/faces/twitter/geneseleznev/128.jpg'
- *      401:
- *        $ref: '#/components/responses/UnauthorizedError'
- *      403:
- *        $ref: '#/components/responses/UnauthorizedError'
- */
 router.get(
   "/",
   authRequired,
@@ -97,45 +38,10 @@ router.post("/", authRequired, (req, res) => {
     });
 });
 
-/**
- * @swagger
- * components:
- *  parameters:
- *    profileId:
- *      name: id
- *      in: path
- *      description: ID of the profile to return
- *      required: true
- *      example: 00uhjfrwdWAQvD8JV4x6
- *      schema:
- *        type: string
- *
- * /profile/{id}:
- *  get:
- *    description: Find Users by ID
- *    summary: Returns a single profile
- *    security:
- *      - okta: []
- *    tags:
- *      - profile
- *    parameters:
- *      - $ref: '#/components/parameters/profileId'
- *    responses:
- *      200:
- *        description: A profile object
- *        content:
- *          application/json:
- *            schema:
- *              $ref: '#/components/schemas/Profile'
- *      401:
- *        $ref: '#/components/responses/UnauthorizedError'
- *      404:
- *        description: 'Profile not found'
- */
 router.get(
   "/:id",
   authRequired,
-  restrictTo("executive_director", "case_manager", "supervisor"),
+  restrictTo("admin"),
   (req, res) => {
     const id = String(req.params.id);
     Users.findById(id)
@@ -152,96 +58,7 @@ router.get(
   }
 );
 
-router.get("/:id/family", authRequired, async (req, res) => {
-  console.log("hit");
-  try {
-    const family = await Users.findFamilyByUserId(req.params.id);
 
-    console.log(family);
-
-    if (!family) {
-      res.status(404).json({ message: "Unable to find family for that user" });
-    }
-
-    res.status(200).json({
-      family: family[0],
-    });
-  } catch (error) {
-    res.status(500).json({ message: "Internal Server Error" });
-  }
-});
-
-/**
- * @swagger
- * /profile:
- *  post:
- *    summary: Add a profile
- *    security:
- *      - okta: []
- *    tags:
- *      - profile
- *    requestBody:
- *      description: Profile object to to be added
- *      content:
- *        application/json:
- *          schema:
- *            $ref: '#/components/schemas/Profile'
- *    responses:
- *      400:
- *        $ref: '#/components/responses/BadRequest'
- *      401:
- *        $ref: '#/components/responses/UnauthorizedError'
- *      404:
- *        description: 'Profile not found'
- *      200:
- *        description: A profile object
- *        content:
- *          application/json:
- *            schema:
- *              type: object
- *              properties:
- *                message:
- *                  type: string
- *                  description: A message about the result
- *                  example: profile created
- *                profile:
- *                  $ref: '#/components/schemas/Profile'
- */
-
-/**
- * @swagger
- * /profile:
- *  put:
- *    summary: Update a profile
- *    security:
- *      - okta: []
- *    tags:
- *      - profile
- *    requestBody:
- *      description: Profile object to to be updated
- *      content:
- *        application/json:
- *          schema:
- *            $ref: '#/components/schemas/Profile'
- *    responses:
- *      401:
- *        $ref: '#/components/responses/UnauthorizedError'
- *      404:
- *        $ref: '#/components/responses/NotFound'
- *      200:
- *        description: A profile object
- *        content:
- *          application/json:
- *            schema:
- *              type: object
- *              properties:
- *                message:
- *                  type: string
- *                  description: A message about the result
- *                  example: profile created
- *                profile:
- *                  $ref: '#/components/schemas/Profile'
- */
 router.put(
   "/:id",
   authRequired,
@@ -272,37 +89,8 @@ router.put(
       });
   }
 );
-/**
- * @swagger
- * /profile/{id}:
- *  delete:
- *    summary: Remove a profile
- *    security:
- *      - okta: []
- *    tags:
- *      - profile
- *    parameters:
- *      - $ref: '#/components/parameters/profileId'
- *    responses:
- *      401:
- *        $ref: '#/components/responses/UnauthorizedError'
- *      404:
- *        $ref: '#/components/responses/NotFound'
- *      200:
- *        description: A profil object
- *        content:
- *          application/json:
- *            schema:
- *              type: object
- *              properties:
- *                message:
- *                  type: string
- *                  description: A message about the result
- *                  example: Profile '00uhjfrwdWAQvD8JV4x6' was deleted.
- *                profile:
- *                  $ref: '#/components/schemas/Profile'
- */
-router.delete("/:id", restrictTo("executive_director"), (req, res) => {
+
+router.delete("/:id", restrictTo("admin"), (req, res) => {
   const { id } = req.params;
   try {
     Users.findById(id).then((profile) => {
