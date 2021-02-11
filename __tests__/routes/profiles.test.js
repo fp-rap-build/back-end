b04479183676c081ddd8bea519cbc4a146804c18
Christopher Harwell <chrisharwell72@gmail.com>
2021-02-11 17:18:45 -0500

    Adds generator file for testing

--- __tests__/routes/profiles.test.js
+++ __tests__/routes/profiles.test.js
@@ -1,94 +1,36 @@
 const request = require("supertest");
 const express = require("express");
-const Profiles = require("../../api/profile/profileModel");
-const profileRouter = require("../../api/profile/profileRouter");
+const Users = require("../../api/routes/users/userModel");
+const userRouter = require("../../api/routes/users/userRouter");
 
 const server = express();
 server.use(express.json());
 
-jest.mock("../../api/profile/profileModel");
-// mock the auth middleware completely
-jest.mock("../../api/middleware/authRequired", () =>
-  jest.fn((req, res, next) => next())
-);
+const client = new okta.Client({
+	orgUrl: "https://okta-dev-79515564.okta.com",
+	authorizationMode: "PrivateKey",
+	clientId: "0oa4nwcyb3w5UdWlb5d6",
+	scopes: ["okta.users.manage"],
+	privateKey: "{00WzfwJ9bDJJ41AuFZF8D5gJtwXaFL1CpBoR3co8WJ}", // <-- see notes below
+});
 
-describe("profiles router endpoints", () => {
-  beforeAll(() => {
-    // This is the module/route being tested
-    server.use(["/profile", "/profiles"], profileRouter);
-    jest.clearAllMocks();
-  });
+// Mock authRequired middleware
+jest.mock("../../api/middleware/authRequired.js", () => {
+	jest.fn((req, res, next) => next());
+});
 
-  describe("GET /profiles", () => {
-    it("should return 200", async () => {
-      Profiles.findAll.mockResolvedValue([]);
-      const res = await request(server).get("/profiles");
+jest.mock("../../api/routes/users/userModel.js", () => {
+	jest.fn((req, res, next) => next());
+});
 
-      expect(res.status).toBe(200);
-      expect(res.body.length).toBe(0);
-      expect(Profiles.findAll.mock.calls.length).toBe(1);
-    });
-  });
-
-  describe("GET /profiles/:id", () => {
-    it("should return 200 when profile found", async () => {
-      Profiles.findById.mockResolvedValue({
-        id: "d376de0577681ca93614",
-        name: "Bob Smith",
-        email: "bob@example.com",
+// Test users/me route
+describe("server.js", () => {
+        let res = {};
+        beforeAll(async () => {
+            res = await request(userRouter).get("/me");
+        })
+      it("Should return 200 ok", async () => {
+        // const res = await request(server).get("/");
+        expect(res.status).toBe(200);
       });
-      const res = await request(server).get("/profiles/d376de0577681ca93614");
-
-      expect(res.status).toBe(200);
-      expect(res.body.name).toBe("Bob Smith");
-      expect(Profiles.findById.mock.calls.length).toBe(1);
-    });
-
-    it("should return 404 when no user found", async () => {
-      Profiles.findById.mockResolvedValue();
-      const res = await request(server).get("/profiles/d376de0577681ca93614");
-
-      expect(res.status).toBe(404);
-      expect(res.body.error).toBe("ProfileNotFound");
-    });
-  });
-
-  describe("POST /profile", () => {
-    it("should return 200 when profile is created", async () => {
-      const profile = {
-        name: "Louie Smith",
-        email: "louie@example.com",
-        avatarUrl:
-          "https://s3.amazonaws.com/uifaces/faces/twitter/hermanobrother/128.jpg",
-      };
-      Profiles.findById.mockResolvedValue(undefined);
-      Profiles.create.mockResolvedValue([
-        { id: "d376de0577681ca93614", ...profile },
-      ]);
-      const res = await request(server).post("/profile").send(profile);
-
-      expect(res.status).toBe(200);
-      expect(res.body.profile.id).toBe("d376de0577681ca93614");
-      expect(Profiles.create.mock.calls.length).toBe(1);
-    });
-  });
-
-  describe("PUT /profile", () => {
-    it("should return 200 when profile is created", async () => {
-      const profile = {
-        id: "d376de0577681ca93614",
-        name: "Louie Smith",
-        email: "louie@example.com",
-        avatarUrl:
-          "https://s3.amazonaws.com/uifaces/faces/twitter/hermanobrother/128.jpg",
-      };
-      Profiles.findById.mockResolvedValue(profile);
-      Profiles.update.mockResolvedValue([profile]);
-
-      const res = await request(server).put("/profile/").send(profile);
-      expect(res.status).toBe(200);
-      expect(res.body.profile.name).toBe("Louie Smith");
-      expect(Profiles.update.mock.calls.length).toBe(1);
-    });
-  });
 });
--- api/routes/users/userRouter.js
+++ api/routes/users/userRouter.js
@@ -1,17 +1,17 @@
-const express = require('express');
-const authRequired = require('../../middleware/authRequired');
-const Users = require('./userModel');
+const express = require("express");
+const authRequired = require("../../middleware/authRequired");
+const Users = require("./userModel");
 const router = express.Router();
-const restrictTo = require('../../middleware/restrictTo');
+const restrictTo = require("../../middleware/restrictTo");
 
-router.get('/me', authRequired, (req, res) => {
+router.get("/me", authRequired, (req, res) => {
 	const { user } = req;
 	res.status(200).json({
-		user
+		user,
 	});
 });
 
-router.get('/', authRequired, restrictTo('admin'), async (req, res) => {
+router.get("/", authRequired, restrictTo("admin"), async (req, res) => {
 	try {
 		let users = await Users.findAll();
 		res.status(200).json(users);
@@ -20,35 +20,45 @@ router.get('/', authRequired, restrictTo('admin'), async (req, res) => {
 	}
 });
 
-router.get('/requests', authRequired, restrictTo('admin', 'program_manager'), async (req, res) => {
-	try {
+router.get(
+	"/requests",
+	authRequired,
+	restrictTo("admin", "program_manager"),
+	async (req, res) => {
+		try {
+			let users = await Users.findAll({
+				is_requesting_assistance: true,
+			});
 
-		let users = await Users.findAll({ is_requesting_assistance: true });
-
-		res.status(200).json(users);
-	} catch (error) {
-		res.status(500).json({ message: error.message });
+			res.status(200).json(users);
+		} catch (error) {
+			res.status(500).json({ message: error.message });
+		}
 	}
-});
+);
 
-router.post('/', authRequired, (req, res) => {
+router.post("/", authRequired, (req, res) => {
 	Users.findOrCreateProfile(req.body)
 		.then(() => {
-			res.status(201).json({ message: 'Profile created' });
+			res.status(201).json({ message: "Profile created" });
 		})
 		.catch(() => {
-			res.status(500).json({ message: 'Unable to create profile' });
+			res.status(500).json({
+				message: "Unable to create profile",
+			});
 		});
 });
 
-router.get('/:id', authRequired, restrictTo('admin'), (req, res) => {
+router.get("/:id", authRequired, restrictTo("admin"), (req, res) => {
 	const id = String(req.params.id);
 	Users.findById(id)
 		.then((profile) => {
 			if (profile) {
 				res.status(200).json(profile);
 			} else {
-				res.status(404).json({ error: 'ProfileNotFound' });
+				res.status(404).json({
+					error: "ProfileNotFound",
+				});
 			}
 		})
 		.catch((err) => {
@@ -56,73 +66,90 @@ router.get('/:id', authRequired, restrictTo('admin'), (req, res) => {
 		});
 });
 
+router.get(
+	"/:id/address",
+	authRequired,
+	restrictTo("admin"),
+	async (req, res) => {
+		let { id } = req.params;
+		try {
+			let address = await Users.findAddressByUserId(id);
+			res.status(200).json({ address: address[0] });
+		} catch (error) {
+			res.status(500).json({ message: error.message });
+		}
+	}
+);
 
-router.get('/:id/address', authRequired, restrictTo('admin'), async (req, res) => {
-  let { id } = req.params
-  try {
-    let address = await Users.findAddressByUserId(id)
-    res.status(200).json({ address: address[0]})
-  } catch (error) {
-    res.status(500).json({ message: error.message})
-  }
-});
+router.put(
+	"/:id/address",
+	authRequired,
+	restrictTo("admin"),
+	async (req, res) => {
+		let { id } = req.params;
 
-router.put('/:id/address', authRequired, restrictTo('admin'), async (req, res) => {
-  let { id } = req.params
+		// Make it impossible to update the id
+		req.body["id"] = undefined;
 
-  // Make it impossible to update the id
-  req.body['id'] = undefined
+		let payload = req.body;
 
-  let payload = req.body
+		try {
+			let user = await Users.findById(id);
 
-  try {
-    let user = await Users.findById(id)
+			let updatedAddress = await Users.updateAddressById(
+				user.address_id,
+				payload
+			);
 
-    let updatedAddress = await Users.updateAddressById(user.address_id, payload )
+			res.status(200).json({ address: updatedAddress[0] });
+		} catch (error) {
+			res.status(500).json({ message: error.message });
+		}
+	}
+);
 
-    res.status(200).json({ address: updatedAddress[0] })
-
-  } catch (error) {
-    res.status(500).json({ message: error.message})
-  }
-})
-
-router.put('/:id', authRequired, restrictTo('admin'), (req, res) => {
+router.put("/:id", authRequired, restrictTo("admin"), (req, res) => {
 	const profile = req.body;
 	const { id } = req.params;
 	Users.findById(id)
 		.then(
 			Users.update(id, profile)
 				.then((updated) => {
-					res.status(200).json({ message: 'profile updated', profile: updated[0] });
+					res.status(200).json({
+						message: "profile updated",
+						profile: updated[0],
+					});
 				})
 				.catch((err) => {
 					res.status(500).json({
 						message: `Could not update profile '${id}'`,
-						error: err.message
+						error: err.message,
 					});
 				})
 		)
 		.catch((err) => {
 			res.status(404).json({
 				message: `Could not find profile '${id}'`,
-				error: err.message
+				error: err.message,
 			});
 		});
 });
 
-router.delete('/:id', restrictTo('admin'), (req, res) => {
+router.delete("/:id", restrictTo("admin"), (req, res) => {
 	const { id } = req.params;
 	try {
 		Users.findById(id).then((profile) => {
 			Users.remove(profile.id).then(() => {
-				res.status(200).json({ message: `Profile '${id}' was deleted.`, profile });
+				res.status(200).json({
+					message: `Profile '${id}' was deleted.`,
+					profile,
+				});
 			});
 		});
 	} catch (err) {
 		res.status(500).json({
 			message: `Could not delete profile with ID: ${id}`,
-			error: err.message
+			error: err.message,
 		});
 	}
 });
