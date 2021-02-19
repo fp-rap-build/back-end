1916c10aac223ca23cdb68dee02f665d2e90b238
joeywire <61367542+joeywire@users.noreply.github.com>
2021-02-19 17:37:39 -0600

    updating address seeds

--- data/seeds/01-addresses.js
+++ data/seeds/01-addresses.js
@@ -1,32 +1,14 @@
 exports.seed = function (knex) {
   // Deletes ALL existing entries
-  return knex("addresses")
+  return knex('addresses')
     .del()
     .then(function () {
       // Inserts seed entries
-      return knex("addresses").insert([
+      return knex('addresses').insert([
         {
-          address: "904 E. Hartson Ave",
-          cityName: "Spokane",
-          state: "WA",
-          zipCode: 99202,
-        },
-        {
-          address: "904 E. Hartson Ave",
-          cityName: "Spokane",
-          state: "WA",
-          zipCode: 99202,
-        },
-        {
-          address: "904 E. Hartson Ave",
-          cityName: "Spokane",
-          state: "WA",
-          zipCode: 99202,
-        },
-        {
-          address: "904 E. Hartson Ave",
-          cityName: "Spokane",
-          state: "WA",
+          address: '904 E. Hartson Ave',
+          cityName: 'Spokane',
+          state: 'WA',
           zipCode: 99202,
         },
       ]);
