c83f26c8213c060f9a709e934e8853dfa07f3f77
joeywire <61367542+joeywire@users.noreply.github.com>
2021-02-19 18:28:54 -0600

    fix users/me test

--- __tests__/routes/profiles.test.js
+++ __tests__/routes/profiles.test.js
@@ -2,7 +2,6 @@ const request = require('supertest');
 const express = require('express');
 const Users = require('../../api/routes/users/userModel');
 const userRouter = require('../../api/routes/users/userRouter');
-const generatorData = require('../../api/generatorData');
 
 const server = express();
 server.use(express.json());
@@ -11,7 +10,7 @@ server.use(express.json());
 
 // Mock authRequired middleware
 jest.mock('../../api/middleware/authRequired', () => {
-  jest.fn((req, res, next) => next());
+  return jest.fn((req, res, next) => next());
 });
 
 // Mock the database
