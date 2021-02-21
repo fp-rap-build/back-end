e9b38b3cc2bd8c14628d3488b60e3d46c48f5997
Isaiah <greatpup1940@gmail.com>
2021-02-21 04:31:58 -0500

    Created authRequired middleware for protecting routes

--- api/middleware/authRequired.js
+++ api/middleware/authRequired.js
@@ -1,75 +1,39 @@
-const createError = require('http-errors');
-const OktaJwtVerifier = require('@okta/jwt-verifier');
-const oktaVerifierConfig = require('../../config/okta');
-const Users = require('../routes/users/userModel');
 
-const oktaJwtVerifier = new OktaJwtVerifier(oktaVerifierConfig.config);
-
-const makeProfileObj = (claims) => {
-  let id,
-    email,
-    firstName,
-    lastName = '';
-
-  if (claims.firstName) {
-    firstName = claims.firstName;
-  }
-
-  if (claims.lastName) {
-    lastName = claims.lastName;
-  }
-
-  if (claims.name) {
-    firstName = claims.name.split(' ')[0];
-  }
-
-  id = claims.sub;
-  email = claims.email;
-
-  const profile = {
-    id,
-    email,
-    firstName,
-    lastName,
-  };
-
-  return profile;
-};
-
-/**
- * A simple middleware that asserts valid Okta idToken and sends 401 responses
- * if the token is not present or fails validation. If the token is valid its
- * contents are attached to req.profile
- */
+const User = require('../routes/users/userModel');
+const jwt = require('jsonwebtoken')
+const { promisify } = require('util');
 
 const authRequired = async (req, res, next) => {
-  try {
-    const authHeader = req.headers.authorization || '';
-    const match = authHeader.match(/Bearer (.+)/);
-
-    if (!match) throw new Error('Missing idToken');
-
-    const idToken = match[1];
-
-    oktaJwtVerifier
-      .verifyAccessToken(idToken, oktaVerifierConfig.expectedAudience)
-      .then(async (data) => {
-        const jwtUserObj = makeProfileObj(data.claims);
-        const user = await Users.findOrCreateProfile(jwtUserObj);
-        if (user) {
-          req.user = user;
-        } else {
-          throw new Error('Unable to process idToken');
-        }
-        next();
-      })
-      .catch((e) => {
-        console.log(e);
-        res.status(401).json({ message: 'Invalid token' });
-      });
-  } catch (err) {
-    next(createError(401, err.message));
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
   }
+
+  if (!token) {
+    return res.status(401).json({ message: 'You are not logged in! Please log in to get access' })  
+  }
+
+  // 2) Verification token
+
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+  // 3) Check if user still exists
+  const currentUser = await User.findById(decoded.id);
+
+  if (!currentUser) {
+   return res.status(401).json({ message: "The user belonging to this token no longer exists" })
+  }
+
+  // Hide password
+  
+  currentUser['password'] = undefined
+
+  // GRANT ACCESS TO PROTECTED ROUTE
+  req.user = currentUser;
+  next();
 };
 
 module.exports = authRequired;
