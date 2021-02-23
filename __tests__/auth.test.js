942813203ab37f5c77ccc99ad1f25fa9c5a87467
Isaiah <greatpup1940@gmail.com>
2021-02-23 00:02:25 -0500

    Testing input validation for /auth/register

--- __tests__/auth.test.js
+++ __tests__/auth.test.js
@@ -3,6 +3,11 @@ const supertest = require('supertest');
 const app = require('../api/app'); // the express server
 const db = require('../data/db-config');
 
+beforeAll(async () => {
+  await db.migrate.rollback();
+  await db.migrate.latest();
+});
+
 beforeEach(async () => {
   await db.seed.run();
   //Seeds add 4 addresses to table
@@ -11,7 +16,7 @@ afterAll(async () => {
   await db.destroy();
 });
 
-describe('POST /auth', () => {
+describe('Authentication routes', () => {
   describe('POST /auth/register', () => {
     const testUser = {
       firstName: 'test',
@@ -22,16 +27,15 @@ describe('POST /auth', () => {
       familySize: 2,
       monthlyIncome: 500,
       isRequestingAssistance: true,
-
     };
 
-    it('Responds with json', async () => {
+    it('Works as expected', async () => {
       let res = await supertest(app).post('/auth/register').send(testUser);
 
       expect(res.type).toBe('application/json');
       expect(res.status).toBe(200);
 
-      // Verify user body
+      // Verify response body
       const {
         firstName,
         lastName,
@@ -43,44 +47,62 @@ describe('POST /auth', () => {
         isRequestingAssistance,
       } = res.body.user;
 
-      const { token, status } = res.body
+      const { token, status } = res.body;
 
-      expect(token).toBeTruthy()
-      expect(status).toBe('success')
+      expect(token).toBeTruthy();
+      expect(status).toBe('success');
 
       expect(firstName).toBe('test');
       expect(lastName).toBe('test');
       expect(email).toBe('testemail@gmail.com');
-      expect(role).toBe('tenant')
+      expect(role).toBe('tenant');
       expect(addressId).toBeTruthy();
       expect(requestStatus).toBe('received');
-      expect(isRequestingAssistance).toBe(true)
-      expect(monthlyIncome).toBe(500)
+      expect(isRequestingAssistance).toBe(true);
+      expect(monthlyIncome).toBe(500);
     });
 
     const existingUser = {
-      firstName: "John",
-      lastName: "shelby",
-      email: "tenant@gmail.com",
-      password: "testpassword"
-    }
+      firstName: 'John',
+      lastName: 'shelby',
+      email: 'tenant@gmail.com',
+      password: 'testpassword',
+      role: 'tenant',
+      monthlyIncome: 100,
+      familySize: 3,
+    };
 
     it('Responds with 400 when user already exists', async () => {
-      let res = await supertest(app).post('/auth/register').send(existingUser)
+      let res = await supertest(app).post('/auth/register').send(existingUser);
 
-      expect(res.type).toBe('application/json')
-      expect(res.status).toBe(400)
-      expect(res.body.message).toBe('User with that email already exists')
-    })
+      expect(res.type).toBe('application/json');
+      expect(res.status).toBe(400);
+      expect(res.body.message).toBe('User with that email already exists');
+    });
 
-    it('Responds with 400 when missing fields', () => {
-      let res = await supertest(app).post({})
+    it('Responds with 422 when missing fields', async () => {
+      let res = await supertest(app).post('/auth/register').send({});
 
-      expect(res.type).toBe('application/json')
-      expect(res.status).toBe(400)
-      
+      expect(res.type).toBe('application/json');
+      expect(res.status).toBe(422);
+      expect(res.body.errors).toBeTruthy();
+    });
 
-    })
+    it('Reponds with 422 when role is anything but landlord or tenant', async () => {
+      testUser['role'] = 'admin';
 
+      let res = await supertest(app).post('/auth/register').send(testUser);
+
+      // Switch role back to tenant for any future tests
+      testUser['role'] = 'tenant';
+
+      expect(res.type).toBe('application/json');
+      expect(res.status).toBe(422);
+      expect(res.body.errors).toBeTruthy();
+    });
+  });
+
+  describe('POST /auth/login', () => {
+    expect(2 + 2).toBe(4);
   });
 });
