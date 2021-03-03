e73d3e73f2f12fb0c4e490b8db7babd379de28a5
joeywire <61367542+joeywire@users.noreply.github.com>
2021-03-03 12:35:06 -0600

    seeds built - new db schema functional

--- data/migrations/20210303114141_comments.js
+++ data/migrations/20210303114141_comments.js
@@ -1,8 +1,8 @@
 exports.up = function (knex) {
   return knex.schema.createTable('comments', (tbl) => {
     tbl.increments('id');
-    tbl.integer('request_id').references('id').inTable('requests');
-    tbl.uuid('author_id').references('id').inTable('users');
+    tbl.integer('requestId').references('id').inTable('requests');
+    tbl.uuid('authorId').references('id').inTable('users');
     tbl.text('comment').notNullable();
   });
 };
