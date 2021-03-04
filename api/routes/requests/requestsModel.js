4b210cd2d3ec4271e511ea8ff33af08e1a7a4d02
joeywire <61367542+joeywire@users.noreply.github.com>
2021-03-04 11:39:59 -0600

    requests table CRUD enpoints build

--- api/routes/requests/requestsModel.js
+++ api/routes/requests/requestsModel.js
@@ -1,9 +1,20 @@
 const db = require('../../../data/db-config');
 
-const findAll = async () => 
-  await db('requests as r')
+const findAll = async () => await db('requests as r');
 
+const create = (request) => {
+  return db('requests').insert(request).returning('*');
+};
+
+const remove = async (id) => await db('requests').where({ id }).del();
+
+const update = (id, request) => {
+  return db('requests').where({ id }).first().update(request).returning('*');
+};
 
 module.exports = {
-  findAll
-}
+  findAll,
+  create,
+  remove,
+  update,
+};
--- api/routes/requests/requestsRouter.js
+++ api/routes/requests/requestsRouter.js
@@ -2,13 +2,49 @@ const express = require('express');
 const authRequired = require('../../middleware/authRequired');
 const Requests = require('./requestsModel');
 const restrictTo = require('../../middleware/restrictTo');
+const { update } = require('../../../data/db-config');
 
 const router = express.Router();
 
 router.get('/', async (req, res) => {
   try {
-    const reqs = await Requests.findAll();
-    res.status(200).json({ requests: reqs });
+    const allRequests = await Requests.findAll();
+    res.status(200).json({ requests: allRequests });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const request = req.body;
+    const newRequest = await Requests.create(request);
+    res.status(200).json(newRequest);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+router.put('/', async (req, res) => {
+  try {
+    const change = req.body;
+    const updatedRequest = await Requests.update(change.id, change);
+    res.status(200).json(updatedRequest);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+router.delete('/', async (req, res) => {
+  try {
+    const { id } = req.body;
+    await Requests.remove(id);
+    res
+      .status(200)
+      .json({ message: `Requests with id: ${id} succesfully deleted` });
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: 'Internal server error' });
