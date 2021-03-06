4b18d51a84430f5b31f53e381ac334931b6ad7a2
Isaiah <greatpup1940@gmail.com>
2021-03-06 06:46:18 -0500

    Validating request id

--- api/routes/documents/index.js
+++ api/routes/documents/index.js
@@ -4,8 +4,13 @@ const router = express.Router({ mergeParams: true });
 // Middlewares
 const utils = require('./utils');
 
+// Validators
+const { validateRequestId } = require('./validators');
+
 // Controllers
 const { getAllDocuments, createDocument } = require('./controllers');
+
+router.use(validateRequestId);
 
 // Routes
 router.route('/').post(createDocument).get(getAllDocuments);
