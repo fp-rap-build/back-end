9d11ba52e311a22840a95e6e82599a08bb9f3e8e
Isaiah <greatpup1940@gmail.com>
2021-03-13 05:08:50 -0500

    Added verifiedDocuments column to findForTable join

--- api/routes/requests/requestsModel.js
+++ api/routes/requests/requestsModel.js
@@ -3,72 +3,73 @@ const db = require('../../../data/db-config');
 const findAll = async () => await db('requests');
 
 const create = (request) => {
-	return db('requests').insert(request).returning('*');
+  return db('requests').insert(request).returning('*');
 };
 
 const remove = async (id) => await db('requests').where({ id }).del();
 
 const update = (id, request) => {
-	return db('requests').where({ id }).first().update(request).returning('*');
+  return db('requests').where({ id }).first().update(request).returning('*');
 };
 
 const findAllActive = () => {
-	return db('requests as r')
-		.join('users as tenant', 'r.tenantId', '=', 'tenant.id')
-		.join('users as landlord', 'r.landlordId', '=', 'landlord.id')
-		.select(
-			'r.id',
-			'tenant.firstName as tFirstName',
-			'tenant.lastName as tLastName',
-			'landlord.firstName as llFirstName',
-			'landlord.lastName as llLastName',
-			'r.requestStatus',
-			'r.requestDate',
-			'r.apmApproval',
-			'r.pmApproval',
-			'r.bookKeeperApproval',
-			'r.headAcctApproval',
-			'r.adminApproval'
-		)
-		.whereNot('r.requestStatus', 'denied');
+  return db('requests as r')
+    .join('users as tenant', 'r.tenantId', '=', 'tenant.id')
+    .join('users as landlord', 'r.landlordId', '=', 'landlord.id')
+    .select(
+      'r.id',
+      'tenant.firstName as tFirstName',
+      'tenant.lastName as tLastName',
+      'landlord.firstName as llFirstName',
+      'landlord.lastName as llLastName',
+      'r.requestStatus',
+      'r.requestDate',
+      'r.apmApproval',
+      'r.pmApproval',
+      'r.bookKeeperApproval',
+      'r.headAcctApproval',
+      'r.adminApproval'
+    )
+    .whereNot('r.requestStatus', 'denied');
 };
 
 const findForTable = () => {
-	return db('requests as r')
-		.join('addresses as a', 'r.addressId', '=', 'a.id')
-		.join('users as u', 'r.userId', '=', 'u.id')
-		.select(
-			'r.id',
-			'u.firstName',
-			'u.lastName',
-			'u.email',
-			'u.role',
+  return db('requests as r')
+    .join('addresses as a', 'r.addressId', '=', 'a.id')
+    .join('users as u', 'r.userId', '=', 'u.id')
+    .select(
+      'r.id',
+      'u.firstName',
+      'u.lastName',
+      'u.email',
+      'u.role',
       'r.familySize',
       'r.monthlyIncome',
-			'r.requestStatus',
-			'r.requestDate',
-			'r.apmApproval',
-			'r.pmApproval',
-			'r.bookKeeperApproval',
-			'r.headAcctApproval',
-			'r.adminApproval',
-			'a.address',
-			'a.zipCode',
-			'a.cityName',
-			'a.state'
-		);
+      'r.requestStatus',
+      'r.requestDate',
+      'r.apmApproval',
+      'r.pmApproval',
+      'r.bookKeeperApproval',
+      'r.headAcctApproval',
+      'r.adminApproval',
+      'r.verifiedDocuments',
+      'a.address',
+      'a.zipCode',
+      'a.cityName',
+      'a.state'
+    );
 };
 
 const findBy = (filter) => {
-	return db('requests').where(filter);
+  return db('requests').where(filter);
 };
 
 module.exports = {
-	findAll,
-	findBy,
-	create,
-	remove,
-	update,
-	findAllActive,
-	findForTable
+  findAll,
+  findBy,
+  create,
+  remove,
+  update,
+  findAllActive,
+  findForTable,
 };
