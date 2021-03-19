46824a9a62ea649d786e1f7f25ea2a1d73802bcb
Isaiah <greatpup1940@gmail.com>
2021-03-19 15:32:22 -0400

    Added route for updating documents

--- api/routes/documents/index.js
+++ api/routes/documents/index.js
@@ -2,14 +2,21 @@ const express = require('express');
 const router = express.Router();
 
 // Controllers
-const { getAllDocuments ,deleteDocument } = require('./controllers');
+const {
+  getAllDocuments,
+  deleteDocument,
+  updateDocument,
+} = require('./controllers');
 
 // Validators
 const { checkIfDocumentExists } = require('./validators');
 
+router.route('/').get(getAllDocuments);
 
-router.route('/').get(getAllDocuments)
-
-router.route('/:id').all(checkIfDocumentExists).delete(deleteDocument);
+router
+  .route('/:id')
+  .all(checkIfDocumentExists)
+  .put(updateDocument)
+  .delete(deleteDocument);
 
 module.exports = router;
