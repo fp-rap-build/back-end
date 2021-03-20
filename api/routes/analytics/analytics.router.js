f4b026bc674f70ce87dc12a882bddb2f18b63a7e
Chris Harwell <chrisharwell72@gmail.com>
2021-03-20 12:08:22 -0400

    Adds sum of families served to analytics

--- api/routes/analytics/analytics.router.js
+++ api/routes/analytics/analytics.router.js
@@ -6,8 +6,9 @@ const router = express.Router()
 
 router.get('/',restrictTo('admin', 'programManager'), async (req, res) => {
     try {
-        const sumApprovedRequests = await Analytics.getRequestStatus();
-        res.status(200).json({sumRequests: sumApprovedRequests});
+        const sumPeopleServed = await Analytics.getNumServed("requestStatus");
+        const sumFamiliesServed = await Analytics.getNumServed("familySize");
+        res.status(200).json({sumPeopleServed: sumPeopleServed, sumFamiliesServed: sumFamiliesServed });
     } catch (err) {
         console.error(err);
         res.status(500).json({message: err.message});
