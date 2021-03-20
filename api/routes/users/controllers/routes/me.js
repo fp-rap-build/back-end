75ed006e3423cf50248a447e648bbad24deeda25
Isaiah <greatpup1940@gmail.com>
2021-03-19 22:51:08 -0400

    Attaching organization to user inside of users/me

--- api/routes/users/controllers/routes/me.js
+++ api/routes/users/controllers/routes/me.js
@@ -1,53 +1,57 @@
 const { request } = require('express');
 const Users = require('../../userModel');
+const Orgs = require('../../../organizations/org-model');
 
 exports.getCurrentUser = async (req, res) => {
-	const { user } = req;
+  let { user } = req;
 
-	try {
-		let requests = await Users.findRequestsByUserId(user.id);
+  try {
+    let requests = await Users.findRequestsByUserId(user.id);
 
-		user['requests'] = requests;
+    let organization = await Orgs.findById(user.organizationId);
 
-		res.status(200).json({
-			user
-		});
-	} catch (error) {
-		res.status(500).json('internal server error');
-	}
+    user['requests'] = requests;
+    user['organization'] = organization;
+
+    res.status(200).json({
+      user,
+    });
+  } catch (error) {
+    res.status(500).json('internal server error');
+  }
 };
 
 exports.updateCurrentUser = async (req, res) => {
-	const { id } = req.user;
+  const { id } = req.user;
 
-	let role = req.body['role'];
+  let role = req.body['role'];
 
-	// Users can't update their role to admin or program manager
+  // Users can't update their role to admin or program manager
 
-	if (role == 'admin' || role == 'programManager') {
-		return res.status(401).json({ message: 'Nice try' });
-	}
+  if (role == 'admin' || role == 'programManager') {
+    return res.status(401).json({ message: 'Nice try' });
+  }
 
-	let payload = req.body;
+  let payload = req.body;
 
-	try {
-		let updatedUser = await Users.findByIdAndUpdate(id, payload);
+  try {
+    let updatedUser = await Users.findByIdAndUpdate(id, payload);
 
-		res.status(200).json({ user: updatedUser[0] });
-	} catch (error) {
-		console.log(error);
-		res.status(500).json({ message: 'Internal server error' });
-	}
+    res.status(200).json({ user: updatedUser[0] });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
 };
 
 exports.deleteCurrentUser = async (req, res, next) => {
-	const { id } = req.user;
+  const { id } = req.user;
 
-	try {
-		await Users.findByIdAndDelete(id);
+  try {
+    await Users.findByIdAndDelete(id);
 
-		res.status(204).json({ message: 'User was successfully deleted' });
-	} catch (error) {
-		res.status(500).json({ message: 'Internal server error' });
-	}
+    res.status(204).json({ message: 'User was successfully deleted' });
+  } catch (error) {
+    res.status(500).json({ message: 'Internal server error' });
+  }
 };
