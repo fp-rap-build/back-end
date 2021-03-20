b897c6a252e046ee4201c69e60986295ca38cffc
joeywire <61367542+joeywire@users.noreply.github.com>
2021-03-20 12:42:31 -0500

    added addrId to requests seeds

--- data/migrations/20210303114141_comments.js
+++ data/migrations/20210303114141_comments.js
@@ -1,7 +1,6 @@
 exports.up = function(knex) {
 	return knex.schema.createTable('comments', (tbl) => {
 		tbl.increments('id');
-    
 		tbl.integer('requestId').references('id').inTable('requests');
 		tbl.uuid('authorId').references('id').inTable('users');
 		tbl.text('comment').notNullable();
