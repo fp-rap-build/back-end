a090a447e16909a0fd60d706085845fb2f657b71
Isaiah <greatpup1940@gmail.com>
2021-03-21 21:54:14 -0400

    Updated analytics to count the familySize

--- api/routes/analytics/analytics.model.js
+++ api/routes/analytics/analytics.model.js
@@ -1,14 +1,23 @@
-const db = require('../../../data/db-config')
+const db = require('../../../data/db-config');
 
 const getFamiliesServed = () => {
-    return db.count("familySize").from("requests").where("requestStatus", "=", "approved");
+  return db
+    .count('requestStatus')
+    .from('requests')
+    .where('requestStatus', '=', 'approved');
 };
 
 const getPeopleServed = () => {
-    return db.count("requestStatus").from("requests").where("requestStatus", "=", "approved");
+  return db
+    .sum('familySize')
+    .from('requests')
+    .where('requestStatus', '=', 'approved');
 };
 
 const getChildrenServed = () => {
-    return db.sum("numChildren").from("requests").where("requestStatus", "=", "approved");
-}
-module.exports = {getFamiliesServed, getPeopleServed, getChildrenServed}
+  return db
+    .sum('numChildren')
+    .from('requests')
+    .where('requestStatus', '=', 'approved');
+};
+module.exports = { getFamiliesServed, getPeopleServed, getChildrenServed };
