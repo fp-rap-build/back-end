b7ca8fdf8ecb31f4477936dc0a82b9e7dda29449
Isaiah Fowler <isaiah@Isaiahs-MacBook-Air.local>
2021-04-07 19:33:38 -0400

    defaulting category to external inside comments table

--- data/migrations/20210328193016_adding_comment_type.js
+++ data/migrations/20210328193016_adding_comment_type.js
@@ -1,6 +1,6 @@
 exports.up = function (knex) {
   return knex.schema.table('comments', (tbl) => {
-    tbl.enu('category', ['internal', 'external']).notNullable();
+    tbl.enu('category', ['internal', 'external']).notNullable().defaultTo('external')
   });
 };
 
