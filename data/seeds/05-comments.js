81f53dd56cb65e72988f919c839143b8ee079eb3
Isaiah Fowler <isaiah@Isaiahs-MacBook-Air.local>
2021-04-07 14:10:21 -0400

    updated seeds to support new user roles

--- data/seeds/05-comments.js
+++ data/seeds/05-comments.js
@@ -2,7 +2,10 @@ const db = require('../db-config');
 
 exports.seed = async function (knex) {
   //see requests seed file for example response from db
-  const ids = await db('users').select('id');
+  const users = await db('users')
+
+  const programManager = users.find(user => user.email === 'programManager@gmail.com')
+  const tenant = users.find(user => user.email === 'tenant@gmail.com')
 
   return knex('comments')
     .del()
@@ -10,14 +13,14 @@ exports.seed = async function (knex) {
       return knex('comments').insert([
         {
           requestId: 1,
-          authorId: ids[0].id,
+          authorId: programManager.id,
           comment:
             'Hi Billy I received your requests and have a few questions!',
           createdAt: '2021-03-20 13:00:00',
         },
         {
           requestId: 1,
-          authorId: ids[2].id,
+          authorId: tenant.id,
           comment: 'Great - What do you need from me?',
           createdAt: '2021-03-20 14:00:00',
         },
